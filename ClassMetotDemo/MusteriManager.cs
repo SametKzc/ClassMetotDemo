d0acff106636c0ca9a297b4bae8335c9eae6a434
Samet <kuzucusamet01@gmail.com>
2023-07-06 13:51:12 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void  MusteriEkle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri Bilgileri Eklendi : " + musteri.Ad + " " + musteri.Soyad);
+        } 
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Bilgileri Listelendi : " + musteri.Id + " " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Bigileri Silindi: " + musteri.Id); 
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassMetotDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Musteri musteri1 = new Musteri();
+        musteri1.Id = 1;    
+        musteri1.Ad = "Mert";
+        musteri1.Soyad = "Demir";   
+
+        MusteriManager musteriManager  = new MusteriManager();
+        musteriManager.MusteriEkle(musteri1);
+        musteriManager.MusteriListele(musteri1);
+        musteriManager.MusteriSil(musteri1);
+       
+    }
+}
